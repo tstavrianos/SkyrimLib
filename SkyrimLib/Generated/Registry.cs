7b4d2f9fd938a49b35e8d93b14b5cfae45484ce7
Theo <tstavrianos@users.noreply.github.com>
2019-09-23 15:12:05 +0100

    Codegen produces one file per class now. The registry class is now generated along with everything else.

--- SkyrimLib/Generated/Registry.cs
+++ SkyrimLib/Generated/Registry.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using SkyrimLib.Generated;
 
 namespace SkyrimLib
 {
@@ -8,15 +7,12 @@ namespace SkyrimLib
     {
         internal static readonly IReadOnlyDictionary<uint, Func<IReader, IReader, Record>> ParsedRecords;
         internal static readonly IReadOnlyDictionary<(uint record, uint subrecord), Func<IReader, IReader, uint, SubRecord>> ParsedSubRecords;
-
         static Registry()
         {
-            ParsedRecords = new Dictionary<uint, Func<IReader, IReader, Record>>
-            {
-                {TES4.FieldType, (headerReader, dataReader) => new TES4(headerReader, dataReader)}
+            ParsedRecords = new Dictionary<uint, Func<IReader, IReader, Record>> {
+                {TES4.FieldType, (headerReader, dataReader) => new TES4(headerReader, dataReader)},
             };
-            ParsedSubRecords = new Dictionary<(uint record, uint subrecord), Func<IReader, IReader, uint, SubRecord>>
-            {
+            ParsedSubRecords = new Dictionary<(uint record, uint subrecord), Func<IReader, IReader, uint, SubRecord>> {
                 {(TES4.FieldType, TES4_HEDR.FieldType), (headerReader, dataReader, size) => new TES4_HEDR(headerReader, dataReader, size)},
                 {(TES4.FieldType, TES4_CNAM.FieldType), (headerReader, dataReader, size) => new TES4_CNAM(headerReader, dataReader, size)},
                 {(TES4.FieldType, TES4_SNAM.FieldType), (headerReader, dataReader, size) => new TES4_SNAM(headerReader, dataReader, size)},
@@ -24,7 +20,7 @@ namespace SkyrimLib
                 {(TES4.FieldType, TES4_DATA.FieldType), (headerReader, dataReader, size) => new TES4_DATA(headerReader, dataReader, size)},
                 {(TES4.FieldType, TES4_ONAM.FieldType), (headerReader, dataReader, size) => new TES4_ONAM(headerReader, dataReader, size)},
                 {(TES4.FieldType, TES4_INTV.FieldType), (headerReader, dataReader, size) => new TES4_INTV(headerReader, dataReader, size)},
-                {(TES4.FieldType, TES4_INCC.FieldType), (headerReader, dataReader, size) => new TES4_INCC(headerReader, dataReader, size)}
+                {(TES4.FieldType, TES4_INCC.FieldType), (headerReader, dataReader, size) => new TES4_INCC(headerReader, dataReader, size)},
             };
         }
     }
